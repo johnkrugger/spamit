d3c1cbf1827852aad29b6cf836278ae77542d6a6
rjpinto <rjpinto@sigrhemaster.fccn.dgae.mec.pt>
2018-06-15 17:20:58 +0100

    Added the function call to send the emails

--- spamit.py
+++ spamit.py
@@ -12,8 +12,9 @@ import email.header
 import psycopg2
 import psycopg2.extras
 
+
 # MIME capable mail server object
-class mailserver(object):
+class Mailserver(object):
 
     def __init__(self, servername, emailaddress):
         self.server = smtplib.SMTP()
@@ -21,32 +22,33 @@ class mailserver(object):
         self.fromaddr = emailaddress
 
     def spam(self, recipients, title, body, filename):
+        msg = MIMEMultipart()
 
-	msg = MIMEMultipart()
+        msg['From'] = self.fromaddr
+        msg['To'] = self.fromaddr
+        msg['Subject'] = email.header.Header(title, 'utf-8').encode()
 
-    	msg['From'] = self.fromaddr
-    	msg['To'] = self.fromaddr
-    	msg['Subject'] = email.header.Header( title, 'utf-8' ).encode()
+        msg.attach(MIMEText(body, 'html'))
 
-    	msg.attach(MIMEText(body, 'html'))
-
-    	if filename:
+        if filename:
             attachment = open(filename, "rb")
             part = MIMEBase('application', 'octet-stream')
-            part.set_payload((attachment).read())
+            part.set_payload(attachment.read())
             encoders.encode_base64(part)
             part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
             msg.attach(part)
 
-
         self.server.connect(self.servername, 25)
+        self.server.sendmail(self.fromaddr, recipients, msg.as_string())
         self.server.quit()
 
+
 # Postgresql data set
-class dataset(object):
-    
+class Dataset(object):
+
     def __init__(self, host, dbname, user, password, query):
-        with psycopg2.connect(host, dbname, user, password, cursor_factory=psycopg2.extras.RealDictCursor) as connection:
+        with psycopg2.connect(host, dbname, user, password,
+                              cursor_factory=psycopg2.extras.RealDictCursor) as connection:
             self.cursor = connection.cursor()
             self.cursor.execute(query)
 
@@ -56,12 +58,10 @@ class dataset(object):
     def __next__(self):
         result = self.cursor.fetchmany(100)
 
-        if( result ):
-            return([ x['email'] for x in result ])
+        if result:
+            return [x['email'] for x in result]
         else:
             raise StopIteration
 
     def next(self):
         return self.__next__()
-
-
